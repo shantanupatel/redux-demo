f424087b15f7917474a82b0b212c9a4b0de99a20
Shantanu Patel <shantu.patel@gmail.com>
2024-12-29 11:28:33 +0530

    Notes for store, action and reducer.

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("From index.js");
+
+/* 
+Store (shop in the case of a cakeshop which keeps all the cakes on the shelf) - Holds the state of your application
+Action (intention to buy cake and reducing the count from available cakes) - Describes the changes in the state of the application
+Reducer (Shopkeeper who gives the cake) - Ties the store and actions together and carries out the state transition depending on the action
+*/
